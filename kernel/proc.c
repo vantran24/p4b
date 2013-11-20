6c39ca4b99de0586ae6af2fbc3d8c199cef89966
Van <vvtran@wisc.edu>
2013-11-19 20:51:31 -0600

    join wait fix

--- kernel/proc.c
+++ kernel/proc.c
@@ -353,7 +353,7 @@ wait(void)
 		havekids = 0;
 		for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
 			//going through the process table
-			if(p->parent != proc) //if it is not the parent
+			if(p->parent != proc || p->clonecalled == 1) //if it is not the parent
 				continue;
 			havekids = 1;//means proc has kids
 			if(p->state == ZOMBIE){//zombie means that it is done running but
